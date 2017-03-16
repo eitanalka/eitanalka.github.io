e7f2ced97b7467f8a16ba9bd8026c21c9c87b5b8
Eitan Alkaslassy <Eitan Alkaslassy>
2017-03-16 16:38:03 -0400

    fullscreen div

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,14 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
-    );
+      <div className="test">
+        <div className="fullscreen blue">
+          test
+        </div>
+        <div className="fullscreen">
+          test
+        </div>
+      </div>
+    )
   }
 }
