8f2c76b2780b59c47e7bc845dc60e52d73fdec89
Eitan Alkaslassy <eitanalka@gmail.com>
2017-03-18 15:46:13 -0400

    added work

--- src/components/work.js
+++ src/components/work.js
@@ -2,20 +2,28 @@ import React from 'react';
 
 const Work = () => {
   return (
-    <div>
+    <div className=" blue-grey lighten-3">
       <div className="flex-column-center">
         <h3>{`<Work/>`}</h3>
+        <div className="flex-row-center">
+          <div className="fill-space">
+            <h4>Random Quote Generator</h4>
+            <p data-height="525" data-theme-id="0" data-slug-hash="RKVKWR" data-default-tab="result" data-user="eitanalka" data-embed-version="2" data-pen-title="Random Quote Generator" className="codepen">See the Pen <a href="http://codepen.io/eitanalka/pen/RKVKWR/">Random Quote Generator</a> by Eitan Alkaslassy (<a href="http://codepen.io/eitanalka">@eitanalka</a>) on <a href="http://codepen.io">CodePen</a>.</p>
+         </div>
+          <div className="fill-space">
+            <h4>Weather App</h4>
+            <p data-height="525" data-theme-id="0" data-slug-hash="PWKWKz" data-default-tab="result" data-user="eitanalka" data-embed-version="2" data-pen-title="Weather App" className="codepen">See the Pen <a href="http://codepen.io/eitanalka/pen/PWKWKz/">Weather App</a> by Eitan Alkaslassy (<a href="http://codepen.io/eitanalka">@eitanalka</a>) on <a href="http://codepen.io">CodePen</a>.</p>
+          </div>
+          <div className="fill-space">
+            <h4>Wikipedia Search</h4>
+            <p data-height="525" data-theme-id="0" data-slug-hash="ZLabOK" data-default-tab="result" data-user="eitanalka" data-embed-version="2" data-pen-title="Wikipedia App" className="codepen">See the Pen <a href="http://codepen.io/eitanalka/pen/ZLabOK/">Wikipedia App</a> by Eitan Alkaslassy (<a href="http://codepen.io/eitanalka">@eitanalka</a>) on <a href="http://codepen.io">CodePen</a>.</p>
+          </div>
+          <div className="fill-space">
+            <h4>Twitch Streamers</h4>
+            <p data-height="525" data-theme-id="0" data-slug-hash="QdOrwg" data-default-tab="result" data-user="eitanalka" data-embed-version="2" data-pen-title="JSON API App" className="codepen">See the Pen <a href="http://codepen.io/eitanalka/pen/QdOrwg/">JSON API App</a> by Eitan Alkaslassy (<a href="http://codepen.io/eitanalka">@eitanalka</a>) on <a href="http://codepen.io">CodePen</a>.</p>
+          </div>
+        </div>
       </div>
-
-        <div>
-<p data-height="575" data-theme-id="0" data-slug-hash="RKVKWR" data-default-tab="result" data-user="eitanalka" data-embed-version="2" data-pen-title="Random Quote Generator" className="codepen">See the Pen <a href="http://codepen.io/eitanalka/pen/RKVKWR/">Random Quote Generator</a> by Eitan Alkaslassy (<a href="http://codepen.io/eitanalka">@eitanalka</a>) on <a href="http://codepen.io">CodePen</a>.</p>        </div>
-        <div>
-          <p data-height="575" data-theme-id="0" data-slug-hash="PWKWKz" data-default-tab="result" data-user="eitanalka" data-embed-version="2" data-pen-title="Weather App" className="codepen">See the Pen <a href="http://codepen.io/eitanalka/pen/PWKWKz/">Weather App</a> by Eitan Alkaslassy (<a href="http://codepen.io/eitanalka">@eitanalka</a>) on <a href="http://codepen.io">CodePen</a>.</p>
-        </div>
-        <div>
-
-        </div>
-
     </div>
   );
 };
